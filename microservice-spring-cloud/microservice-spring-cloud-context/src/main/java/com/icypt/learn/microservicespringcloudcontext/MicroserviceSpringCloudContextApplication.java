cd71f47f7fe69ae7031f57f99df0bc24497b1bac
icypt <icypt_xy@126.com>
2019-02-27 13:55:26 +0800

    spring-cloud-context 项目创建
    spring-cloud-context 项目创建

---
+++ microservice-spring-cloud/microservice-spring-cloud-context/src/main/java/com/icypt/learn/microservicespringcloudcontext/MicroserviceSpringCloudContextApplication.java
@@ -0,0 +1,13 @@
+package com.icypt.learn.microservicespringcloudcontext;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceSpringCloudContextApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceSpringCloudContextApplication.class, args);
+	}
+
+}
