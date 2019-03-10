27fa21c444dd2dabd574d1189e64455f7e8f3391
icypt <icypt_xy@126.com>
2019-03-10 19:26:27 +0800

    hystrix turbine commit

---
+++ microservice-spring-cloud/microservice-hystrix-turbine/src/main/java/com/icypt/learn/MicroserviceHystrixTurbineApplication.java
@@ -0,0 +1,16 @@
+package com.icypt.learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
+
+@SpringBootApplication
+@EnableTurbine
+public class MicroserviceHystrixTurbineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroserviceHystrixTurbineApplication.class, args);
+    }
+
+}
+
