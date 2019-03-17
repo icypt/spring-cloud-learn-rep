fc7bc134cca5a6bf650b340e9165502a8e4e90a3
icypt <icypt_xy@126.com>
2019-03-17 19:41:50 +0800

    config client commit

---
+++ microservice-spring-cloud/microservice-config-client/src/main/java/com/icypt/learn/MicroserviceConfigClientApplication.java
@@ -0,0 +1,13 @@
+package com.icypt.learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceConfigClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceConfigClientApplication.class, args);
+	}
+
+}
---
+++ microservice-spring-cloud/microservice-config-client/src/main/java/com/icypt/learn/controller/TestConfigController.java
@@ -0,0 +1,23 @@
+package com.icypt.learn.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * platform：www.javaroads.com
+ * Author：Javaroads_Jun
+ * createTime： 2019/3/17 17:24
+ * version：1.0
+ * description：
+ */
+@RestController
+public class TestConfigController {
+    @Value("${profile}")
+    private String profile;
+
+    @GetMapping("/getProfile")
+    public String getProfile() {
+        return  profile;
+    }
+}
