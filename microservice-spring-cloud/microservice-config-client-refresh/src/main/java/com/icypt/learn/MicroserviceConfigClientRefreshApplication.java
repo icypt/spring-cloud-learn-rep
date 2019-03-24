2bb5ead8604fdfe656f6313f7b3fc3ddc56443dd
icypt <icypt_xy@126.com>
2019-03-24 22:03:29 +0800

    config refresh

---
+++ microservice-spring-cloud/microservice-config-client-refresh/src/main/java/com/icypt/learn/MicroserviceConfigClientRefreshApplication.java
@@ -0,0 +1,13 @@
+package com.icypt.learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceConfigClientRefreshApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceConfigClientRefreshApplication.class, args);
+	}
+
+}
