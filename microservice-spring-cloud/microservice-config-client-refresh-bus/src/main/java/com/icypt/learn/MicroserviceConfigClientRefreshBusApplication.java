4c079e20db2e15419a2f47f67e8b6b5ecd931813
icypt <icypt_xy@126.com>
2019-03-24 23:33:03 +0800

    config bus refresh

---
+++ microservice-spring-cloud/microservice-config-client-refresh-bus/src/main/java/com/icypt/learn/MicroserviceConfigClientRefreshBusApplication.java
@@ -0,0 +1,13 @@
+package com.icypt.learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceConfigClientRefreshBusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceConfigClientRefreshBusApplication.class, args);
+	}
+
+}
