88c3b6e6eb11af540c7aa6ac83a442ebf3b24b85
guojun.chen <icypt_xy@126.com>
2020-04-06 00:38:58 +0800

    首次提交

---
+++ spring-cloud-learn-hoxton.sr3/spring-cloud-eureka-server/src/main/java/com/icypt/cloud/SCloudEurekaServerApplication.java
@@ -0,0 +1,13 @@
+package com.icypt.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SCloudEurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SCloudEurekaServerApplication.class, args);
+    }
+
+}
