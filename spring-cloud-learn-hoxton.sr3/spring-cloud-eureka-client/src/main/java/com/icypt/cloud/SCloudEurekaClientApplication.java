ec40c16a223d7fb1e263c27db35c25c2b01657b1
guojun.chen <icypt_xy@126.com>
2020-04-06 21:14:09 +0800

    eureka-server、eureka-client提交

---
+++ spring-cloud-learn-hoxton.sr3/spring-cloud-eureka-client/src/main/java/com/icypt/cloud/SCloudEurekaClientApplication.java
@@ -0,0 +1,13 @@
+package com.icypt.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SCloudEurekaClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SCloudEurekaClientApplication.class, args);
+    }
+
+}
--- spring-cloud-learn-hoxton.sr3/spring-cloud-eureka-server/src/main/java/com/icypt/cloud/SCloudEurekaServerApplication.java
+++ spring-cloud-learn-hoxton.sr3/spring-cloud-eureka-server/src/main/java/com/icypt/cloud/SCloudEurekaServerApplication.java
@@ -2,8 +2,10 @@ package com.icypt.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer//启动注册中心
 public class SCloudEurekaServerApplication {
 
     public static void main(String[] args) {
