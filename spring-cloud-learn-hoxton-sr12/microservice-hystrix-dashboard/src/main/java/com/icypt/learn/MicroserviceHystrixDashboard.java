ec377464c29a8d7c64b76db2c0519c5ee3a5462f
chenguojun <icypt_xy@126.com>
2022-05-06 18:01:30 +0800

    hystrix dashboard

---
+++ spring-cloud-learn-hoxton-sr12/microservice-hystrix-dashboard/src/main/java/com/icypt/learn/MicroserviceHystrixDashboard.java
@@ -0,0 +1,13 @@
+package com.icypt.learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+@SpringBootApplication
+@EnableHystrixDashboard
+public class MicroserviceHystrixDashboard {
+    public static void main(String[] args) {
+        SpringApplication.run(MicroserviceHystrixDashboard.class, args);
+    }
+}
--- spring-cloud-learn-hoxton-sr12/microservice-provider-with-hystrix/src/main/java/com/icypt/learn/MicroserviceProviderWithHystrix.java
+++ spring-cloud-learn-hoxton-sr12/microservice-provider-with-hystrix/src/main/java/com/icypt/learn/MicroserviceProviderWithHystrix.java
@@ -1,10 +1,13 @@
 package com.icypt.learn;
 
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -15,4 +18,13 @@ public class MicroserviceProviderWithHystrix {
     public static void main(String[] args) {
         SpringApplication.run(MicroserviceProviderWithHystrix.class,args);
     }
+
+    @Bean
+    public ServletRegistrationBean hystrixMetricsStreamServlet(){
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(new HystrixMetricsStreamServlet());
+        registrationBean.addUrlMappings("/hystrix.stream");
+        return registrationBean;
+    }
 }
+
+
